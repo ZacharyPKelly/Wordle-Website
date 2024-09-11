9f207a59114a7c821ba17ca53a27bc51e2b4302b
ZacharyPKelly <zpkelly@ualberta.ca>
2024-09-11 12:12:53 -0600

    Server Update

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.post('/wordleFormInput', (req, res) => {
 
